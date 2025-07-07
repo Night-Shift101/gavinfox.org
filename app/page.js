13cf95997e86ae84caa073e856e66b7a5b06f4f2
Night-Shift101 <24gavinfox@gmail.com>
2025-07-06 23:52:13 -0400

    Added License to the webpage

--- app/page.js
+++ app/page.js
@@ -15,6 +15,9 @@ export default function Home() {
       <Skills />
       <Projects />
       <Contact />
+      <footer className="footer-copyright">
+          <p><a href="https://gavinfox.org">gavinfox.org</a> © 2025 by <a href="https://github.com/Night-Shift101">Gavin Fox</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0</a></p>
+        </footer>
       <ScrollButtons />
     </main>
   );
